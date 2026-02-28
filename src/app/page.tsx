79374c4b9703e3bdee0fb59a53dae9dfa84ef941
Dmytro <dimon.skorohodov@gmail.com>
2026-02-28 11:50:17 +0200

    iconsSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/Hero";
+import { IconsSection } from "@/components/IconsSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <IconsSection />
       <main id="content" className="min-h-[50vh]" />
     </>
   );
