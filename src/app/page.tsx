3a212e3ec5d42d9cc45c4d79702a812da8334cfa
Marukur0 <workmahro@gmail.com>
2026-03-21 16:16:29 +0200

    feat: refactor Hero component to be data-driven with configurable content, buttons, stats, and social links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,30 @@ import { OurServicesSection } from "@/components/OurServicesSection";
 import { OurGallerySection } from "@/components/OurGallerySection";
 import { OurTeamSection } from "@/components/OurTeamSection";
 import { WhyChooseSection } from "@/components/WhyChooseSection";
+import {
+  homeHeroTitle,
+  homeHeroSubtitle,
+  homeHeroImage,
+  homeHeroButtons,
+  homeHeroStats,
+  homeHeroSocialLinks,
+} from "@/components/hero/homeHeroData";
+
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      <Hero
+        titleContent={homeHeroTitle}
+        subtitle={homeHeroSubtitle}
+        heroImage={homeHeroImage}
+        showButtons
+        buttons={homeHeroButtons}
+        showStats
+        stats={homeHeroStats}
+        showSocialLinks
+        socialLinks={homeHeroSocialLinks}
+      />
       <IconsSection />
       <WhyChooseSection />
       <OurServicesSection />
