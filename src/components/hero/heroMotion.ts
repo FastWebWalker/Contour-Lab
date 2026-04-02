cba8e9dd3f7d7650f57ccb691057cde20d7431e7
Dmytro <dimon.skorohodov@gmail.com>
2026-04-02 18:20:36 +0300

    added basic animtaiton and config, cursor rule

---
+++ src/components/hero/heroMotion.ts
@@ -0,0 +1,16 @@
+/**
+ * Реекспорт з глобального конфігу анімацій (зворотна сумісність).
+ * Нові імпорти: `@/lib/motion`.
+ */
+export {
+  MOTION_EASE,
+  HERO_EASE,
+  motionConfig,
+  sectionTransition,
+  heroTransition,
+  sectionViewport,
+  staggerContainerVariants,
+  staggerGridVariants,
+  fadeUpVariants,
+  fadeUpCardVariants,
+} from "@/lib/motion";
